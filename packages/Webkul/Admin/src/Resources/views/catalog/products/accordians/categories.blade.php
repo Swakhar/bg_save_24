d343e72c0bdd1e28189de7872d2353ad409b65c9
Md. Najmuzzaman <naj06cse@yahoo.com>
2020-06-23 11:38:17 +0600

    Category Select Option On Product Page

--- packages/Webkul/Admin/src/Resources/views/catalog/products/accordians/categories.blade.php
+++ packages/Webkul/Admin/src/Resources/views/catalog/products/accordians/categories.blade.php
@@ -1,16 +1,18 @@
-@if ($categories->count())
+@if (count($categories))
 
 {!! view_render_event('bagisto.admin.catalog.product.edit_form_accordian.categories.before', ['product' => $product]) !!}
 
 <accordian :title="'{{ __('admin::app.catalog.products.categories') }}'" :active="false">
     <div slot="body">
-
-        {!! view_render_event('bagisto.admin.catalog.product.edit_form_accordian.categories.controls.before', ['product' => $product]) !!}
-
-        <tree-view behavior="normal" value-field="id" name-field="categories" input-type="checkbox" items='@json($categories)' value='@json($product->categories->pluck("id"))'></tree-view>
-
-        {!! view_render_event('bagisto.admin.catalog.product.edit_form_accordian.categories.controls.after', ['product' => $product]) !!}
-
+        <div class="control-group select">
+        <select class="control" name="categories" id="id" dff="">
+            @foreach ($categories as $category)
+                <option value="{{ $category->category_id }}" {{ ($category->category_id) == $product->categories->pluck("id")[0] ? 'selected' : '' }}>
+                    {{ ucwords(str_replace(' '," ",str_replace("/"," > ",$category->url_path))) }}
+                    @endforeach
+                </option>
+        </select>
+        </div>
     </div>
 </accordian>
 
