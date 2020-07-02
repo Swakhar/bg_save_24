ee1dbd8de5c9b546f922ed241535d1fd8d742b8f
Hasan Suvo <suvohasan98@gmail.com>
2020-07-02 17:53:59 +0600

    migration file create for customize section

---
+++ packages/Webkul/CMS/src/Database/Migrations/2020_07_02_113840_create_home_customize_section_details_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateHomeCustomizeSectionDetailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('home_customize_section_details', function (Blueprint $table) {
+            $table->integer('display_category_id');
+            $table->integer('master_id')->unsigned();
+            $table->char('display_block_type', 1)->default('1');
+            $table->foreign('master_id')->references('id')
+                ->on('home_customize_section_masters')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('home_customize_section_details');
+    }
+}
