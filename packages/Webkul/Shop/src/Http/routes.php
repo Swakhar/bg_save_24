4f2ddda00e380bfbe00cd2e99ff1255125f4becb
Hasan Suvo <suvohasan98@gmail.com>
2020-07-15 16:49:48 +0600

    home page design responsive

--- packages/Webkul/Shop/src/Http/routes.php
+++ packages/Webkul/Shop/src/Http/routes.php
@@ -323,9 +323,9 @@ Route::group(['middleware' => ['web', 'locale', 'theme', 'currency']], function 
 
         Route::group(['middleware' => ['customer']], function () {
 
-//            Route::get('stores', 'Badenjki\Seller\Http\Controllers\StoreController@index')->defaults('_config', [
-//                'view' => 'shop::customers.account.store.index'
-//            ])->name('customer.store.index');
+            Route::get('stores', 'Badenjki\Seller\Http\Controllers\StoreController@index')->defaults('_config', [
+                'view' => 'shop::customers.account.store.index'
+            ])->name('customer.store.index');
 //
 //            Route::get('stores/create', 'Badenjki\Seller\Http\Controllers\StoreController@create')->defaults('_config', [
 //                'view' => 'shop::customers.account.store.create'
