91ea643e67aa183fc12e72e2eb3b1084036dc50a
Swakhar <swakhar.me@gmail.com>
2020-08-20 12:36:28 +0200

    Added routes for stores

--- packages/Webkul/Shop/src/Http/routes.php
+++ packages/Webkul/Shop/src/Http/routes.php
@@ -76,7 +76,7 @@ Route::group(['middleware' => ['web', 'locale', 'theme', 'currency']], function 
     Route::get('/terms', 'Webkul\Shop\Http\Controllers\StaticPageController@terms')->defaults('_config', [
         'view' => 'shop::policy.terms'
     ])->name('shop.policy.terms');
-    
+
 
     //subscription
     //subscribe
@@ -209,7 +209,7 @@ Route::group(['middleware' => ['web', 'locale', 'theme', 'currency']], function 
         Route::post('login', 'Webkul\Customer\Http\Controllers\SessionController@create')->defaults('_config', [
             'redirect' => 'customer.profile.index'
         ])->name('customer.session.create');
-        
+
 
         // Registration Routes
         //registration form show
@@ -394,14 +394,14 @@ Route::group(['middleware' => ['web', 'locale', 'theme', 'currency']], function 
             Route::get('stores', 'Badenjki\Seller\Http\Controllers\StoreController@index')->defaults('_config', [
                 'view' => 'shop::customers.account.store.index'
             ])->name('customer.store.index');
-//
-//            Route::get('stores/create', 'Badenjki\Seller\Http\Controllers\StoreController@create')->defaults('_config', [
-//                'view' => 'shop::customers.account.store.create'
-//            ])->name('customer.store.create');
-//
-//            Route::post('stores/create', 'Badenjki\Seller\Http\Controllers\StoreController@store')->defaults('_config',[
-//                'redirect' => 'customer.store.index'
-//            ])->name('customer.store.create');
+
+           Route::get('stores/create', 'Badenjki\Seller\Http\Controllers\StoreController@create')->defaults('_config', [
+               'view' => 'shop::customers.account.store.create'
+           ])->name('customer.store.create');
+
+           Route::post('stores/create', 'Badenjki\Seller\Http\Controllers\StoreController@store')->defaults('_config',[
+               'redirect' => 'customer.store.index'
+           ])->name('customer.store.create');
 
             Route::get('stores/edit/{id}', 'Webkul\Customer\Http\Controllers\AddressController@edit')->defaults('_config', [
                 'view' => 'shop::customers.account.store.edit'
