77bf301e69380bada7794739fdd8fd5e6ddb6f50
Swakhar <swakhar.me@gmail.com>
2020-09-03 10:14:40 +0200

    Hyperlink in footer

--- resources/themes/velocity/views/layouts/footer/footer-links/footer-right.blade.php
+++ resources/themes/velocity/views/layouts/footer/footer-links/footer-right.blade.php
@@ -1,7 +1,7 @@
 <div class="col-lg-2 col-md-12 col-sm-12 footer-ct-content">
     <span class="contact_main">EARN WITH US</span>
     <ul class="footer_middle_content">
-        <li><a href="#">Sale On SafeShop</a></li>
-        <li><a href="#">Affiliated</a></li>
+        <li><a href="{{ route('shop.earn.sell') }}">Sale On SafeShop</a></li>
+        <li><a href="{{ route('shop.earn.affiliated') }}">Affiliated</a></li>
     </ul>
 </div>
